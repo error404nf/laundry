009a45b4b19cebad29b25df782b88c123f5b2d42
sgodleon <116541973+sgodleon@users.noreply.github.com>
2022-10-24 11:42:43 +0530

    Update confirm.php

--- modal/confirm.php
+++ modal/confirm.php
@@ -1,5 +1,6 @@
 <div class="modal fade" id="modal-confirm">
 	<div class="modal-dialog">
+		
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
